4d8227b32e5dde1d6bf504e042971a40a0d7f21a
thienthuann61 <thienthuann61@gmail.com>
2022-09-16 10:21:29 +0700

    Dựng khung layout mặc định

--- src/components/layouts/MainLayout/index.js
+++ src/components/layouts/MainLayout/index.js
@@ -1,17 +1,22 @@
+import classNames from "classnames/bind";
+
 import Header from "~/components/layouts/components/Header"
 import Sidebar from "./Sidebar";
+import styles from './MainLayout.module.scss'
+
+const cx = classNames.bind(styles)
 
 function MainLayout({ children }) {
     return (
-        <>
+        <div className={cx('wrapper')}>
             <Header />
-            <div className="container">
+            <div className={cx('container')}>
                 <Sidebar />
-                <div className="content">
+                <div className={cx('content')}>
                     {children}
                 </div>
             </div>
-        </>
+        </div >
     );
 }
 
